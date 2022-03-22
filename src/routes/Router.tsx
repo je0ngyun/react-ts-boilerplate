38c87c78fc311ff7d59c8c926aae8b8bbe7f030c
김정윤 <kjyjh9930@gmail.com>
2022-03-22 13:46:14 +0900

    feat : 기본 ErrorBoundry 컴포넌트 추가

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import ErrorBoundary from '@components/common/ErrorBoundary'
 import Test from '@pages/Home'
 
 const Router = () => {
   return (
     <>
       <Suspense fallback={<></>}>
-        <Routes>
-          <Route path="/" element={<Test />} />
-        </Routes>
+        <ErrorBoundary fullback={<></>}>
+          <Routes>
+            <Route path="/" element={<Test />} />
+          </Routes>
+        </ErrorBoundary>
       </Suspense>
     </>
   )
