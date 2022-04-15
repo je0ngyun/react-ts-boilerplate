22ef643d7efc324a7a92d34eae4e4373bb3cb1ac
김정윤 <kjyjh9930@gmail.com>
2022-04-15 15:42:00 +0900

    feat : 명령형으로 ErrorBoundary를 초기화 가능하게 수정

--- src/components/error/QueryBoundary/index.tsx
+++ src/components/error/QueryBoundary/index.tsx
@@ -1,4 +1,12 @@
-import React, { ReactElement, ReactNode, Suspense } from 'react'
+import React, {
+  ReactElement,
+  ReactNode,
+  Suspense,
+  forwardRef,
+  useRef,
+  Ref,
+  useImperativeHandle,
+} from 'react'
 import { useQueryErrorResetBoundary } from 'react-query'
 import ErrorBoundary from '@components/Error/ErrorBoundary'
 import DefaultErrorFallBack from '@components/Error/DefaultQueryFallBack'
@@ -11,24 +19,37 @@ interface Props {
   ignoreError?: CustomErrorClass[]
 }
 
+interface ResetRef {
+  reset?(): void
+}
+
 const defaultIgnoreError: CustomErrorClass[] = [UnauthorizedError]
 
-const QueryBoundary = ({
-  children,
-  loadingFallback,
-  errorFallback,
-  ignoreError = [],
-}: Props) => {
-  const { reset } = useQueryErrorResetBoundary()
-  return (
-    <ErrorBoundary
-      fallback={errorFallback || <DefaultErrorFallBack />}
-      ignoreError={new Set([...defaultIgnoreError, ...ignoreError])}
-      resetQuery={reset}
-    >
-      <Suspense fallback={loadingFallback || <></>}>{children}</Suspense>
-    </ErrorBoundary>
-  )
-}
+const QueryBoundary = forwardRef(
+  (
+    { children, loadingFallback, errorFallback, ignoreError = [] }: Props,
+    resetRef: Ref<ResetRef>
+  ) => {
+    const { reset } = useQueryErrorResetBoundary()
+    const ref = useRef<ErrorBoundary | null>(null)
+
+    useImperativeHandle(resetRef, () => ({
+      reset: () => ref.current?.resetBoundary(),
+    }))
+
+    return (
+      <ErrorBoundary
+        ref={ref}
+        fallback={errorFallback || <DefaultErrorFallBack />}
+        ignoreError={new Set([...defaultIgnoreError, ...ignoreError])}
+        resetQuery={reset}
+      >
+        <Suspense fallback={loadingFallback || <></>}>{children}</Suspense>
+      </ErrorBoundary>
+    )
+  }
+)
+
+QueryBoundary.displayName = 'QueryBoundary'
 
 export default QueryBoundary
